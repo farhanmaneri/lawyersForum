d2f9fb651dcf3b4a39b2e41f7e45f9779b6cc807
farhanmaneri <farhanmaneri@gmail.com>
2025-08-07 13:21:12 +0500

    Home.css spell corrected

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../styles/home.css'; // ✅ This imports the styles
+import '../styles/Home.css'; // ✅ This imports the styles
 
 function Home() {
   const people = [
